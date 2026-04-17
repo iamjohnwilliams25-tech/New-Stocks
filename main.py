7c8e020aa81b462015b41d91c5218ed0ee1f5002
iamjohnwilliams25-tech <iamjohnwilliams25@gmail.com>
2026-04-17 16:44:42 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import yfinance as yf
+
+app = FastAPI()
+
+stocks = ["RELIANCE.NS","HDFCBANK.NS","ICICIBANK.NS","TATAMOTORS.NS","INFY.NS","SBIN.NS"]
+
+@app.get("/stocks")
+def get_stocks():
+    data = []
+    
+    for s in stocks:
+        df = yf.download(s, period="10d", interval="1d")
+        price = float(df["Close"].iloc[-1])
+        
+        data.append({
+            "stock": s.replace(".NS",""),
+            "buy_price": round(price * 1.01,2),
+            "target": round(price * 1.05,2),
+            "stop_loss": round(price * 0.97,2)
+        })
+    
+    return data
