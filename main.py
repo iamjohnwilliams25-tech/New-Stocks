08bcd04696a4a17c0f38f480ca1fafa9d9854cfb
iamjohnwilliams25-tech <iamjohnwilliams25@gmail.com>
2026-04-17 16:57:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ def get_stocks():
     data = []
     
     for s in stocks:
-        df = yf.download(s, period="10d", interval="1d")
-        price = float(df["Close"].iloc[-1])
+        try:
+            df = yf.download(s, period="10d", interval="1d")
+            
+            if df.empty:
+                continue
+                
+            price = float(df["Close"].iloc[-1])
+            
+            data.append({
+                "stock": s.replace(".NS",""),
+                "buy_price": round(price * 1.01,2),
+                "target": round(price * 1.05,2),
+                "stop_loss": round(price * 0.97,2)
+            })
         
-        data.append({
-            "stock": s.replace(".NS",""),
-            "buy_price": round(price * 1.01,2),
-            "target": round(price * 1.05,2),
-            "stop_loss": round(price * 0.97,2)
-        })
-    
+        except Exception as e:
+            print(f"Error with {s}: {e}")
+            continue
+
     return data
